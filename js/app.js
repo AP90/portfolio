e370ef3f50fc8420d9ad524ba9e6f117d0221449
Adam Pugh <adampugh@zoho.com>
2017-11-01 18:24:36 +0000

    Added project page changes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
 $(".nav-link-1").on("click", function() {
     $(".homepage").fadeOut("slow", function() {
-        $(".workpage").fadeIn("5000ms");
+        $(".project1-page").fadeIn("5000ms");
     });
 })
 
 $(".arrow").on("click", function() {
-    $(".workpage").fadeOut("slow", function() {
-        $(".homepage").fadeIn("5000ms");
+    $(".project1-page, .project2-page, .project3-page, .skillspage").fadeOut("slow", function() {
+        setTimeout(function() {
+            $(".homepage").fadeIn("slow");
+        }, 800)
+    });
+});
+
+
+$(".project-2").on("click", function() {
+    $(".project1-page, .project3-page").fadeOut("slow", function() {
+        setTimeout(function() {
+            $(".project2-page").fadeIn("slow");
+        }, 800)
+    });
+});
+
+$(".project-1").on("click", function() {
+    $(".project2-page, .project3-page").fadeOut("slow", function() {
+        setTimeout(function() {
+            $(".project1-page").fadeIn("slow");
+        }, 800)
+    });
+});
+
+$(".project-3").on("click", function() {
+    $(".project1-page, .project2-page").fadeOut("slow", function() {
+        setTimeout(function() {
+            $(".project3-page").fadeIn("slow");
+        }, 800)
+    });
+});
+
+$(".nav-link-2").on("click", function() {
+    $(".homepage").fadeOut("slow", function() {
+        setTimeout(function() {
+            $(".skillspage").fadeIn("slow");
+        });
     });
 });
