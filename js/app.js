7ca1d3af6e9cf89d30d119802f35d17b8238b70e
Adam Pugh <adampugh@zoho.com>
2017-12-10 23:08:52 +0000

    Added Kuroinu project

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(".nav-link-1").on("click", function() {
 })
 
 $(".arrow").on("click", function() {
-    $(".project1-page, .project2-page, .project3-page, .skillspage, .aboutpage, .contactpage").fadeOut("slow", function() {
+    $(".project1-page, .project2-page, .project3-page, .project4-page, .project5-page, .project6-page, .skillspage, .aboutpage, .contactpage").fadeOut("slow", function() {
         setTimeout(function() {
             $(".homepage").fadeIn("slow");
         }, 800)
@@ -14,7 +14,7 @@ $(".arrow").on("click", function() {
 
 
 $(".project-2").on("click", function() {
-    $(".project1-page, .project3-page").fadeOut("slow", function() {
+    $(".project1-page, .project3-page, .project4-page, .project5-page, .project6-page").fadeOut("slow", function() {
         setTimeout(function() {
             $(".project2-page").fadeIn("slow");
         }, 800)
@@ -22,7 +22,7 @@ $(".project-2").on("click", function() {
 });
 
 $(".project-1").on("click", function() {
-    $(".project2-page, .project3-page").fadeOut("slow", function() {
+    $(".project2-page, .project3-page, .project4-page, .project5-page, .project6-page").fadeOut("slow", function() {
         setTimeout(function() {
             $(".project1-page").fadeIn("slow");
         }, 800)
@@ -30,12 +30,41 @@ $(".project-1").on("click", function() {
 });
 
 $(".project-3").on("click", function() {
-    $(".project1-page, .project2-page").fadeOut("slow", function() {
+    $(".project1-page, .project2-page, .project4-page, .project5-page, .project6-page").fadeOut("slow", function() {
         setTimeout(function() {
             $(".project3-page").fadeIn("slow");
         }, 800)
     });
 });
+
+$(".project-4").on("click", function() {
+    $(".project1-page, .project2-page, .project3-page, .project5-page, .project6-page").fadeOut("slow", function() {
+        setTimeout(function() {
+            $(".project4-page").fadeIn("slow");
+        }, 800)
+    });
+});
+
+$(".project-5").on("click", function() {
+    $(".project1-page, .project2-page, .project3-page, .project4-page, .project6-page").fadeOut("slow", function() {
+        setTimeout(function() {
+            $(".project5-page").fadeIn("slow");
+        }, 800)
+    });
+});
+
+$(".project-6").on("click", function() {
+    $(".project1-page, .project2-page, .project3-page, .project4-page, .project5-page").fadeOut("slow", function() {
+        setTimeout(function() {
+            $(".project6-page").fadeIn("slow");
+        }, 800)
+    });
+});
+
+
+
+
+
 
 $(".nav-link-2").on("click", function() {
     $(".homepage").fadeOut("slow", function() {
